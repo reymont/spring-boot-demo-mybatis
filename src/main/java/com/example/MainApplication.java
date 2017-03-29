249a588a19c8ea4eb84aa6f9d3509c7d7d47c4fb
张开会 <zhangkh@126.com>
2017-03-29 13:43:23 +0800

    将配置文件放到config路径下

--- src/main/java/com/example/MainApplication.java
+++ src/main/java/com/example/MainApplication.java
@@ -4,7 +4,7 @@ import com.example.config.ApplicationConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.example.controllers", "com.example.services"})
 public class MainApplication {
 
 	public static void main(String[] args) {
